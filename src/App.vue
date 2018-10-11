91ea837945bb3fc7f25d5555324291435ab42fad
HuseyinSoyturk <soyturk.huseyin61@gmail.com>
2018-10-11 11:53:46 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Map msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Map from "./components/Map.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Map
   }
-}
+};
 </script>
 
 <style>
+
+body{
+  margin: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+
+#map {
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  position: absolute;
 }
 </style>
---
+++ src/components/Map.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+import Map from "ol/Map";
+import View from "ol/View";
+
+import TileLayer from "ol/layer/Tile";
+import LayerVector from "ol/layer/Vector";
+import SourceVector from "ol/source/Vector";
+import GeoJSON from "ol/format/GeoJSON";
+import Feature from "ol/Feature";
+
+import Image from "ol/layer/Image";
+import ImageWMS from "ol/source/ImageWMS";
+import TileImage from "ol/source/TileImage";
+import OSM from "ol/source/OSM.js";
+
+export default {
+  name: "Map",
+  props: {
+    msg: String
+  },
+  mounted() {
+    new Map({
+      layers: [
+        new TileLayer({
+          title: "Google Yol",
+          // image: "/assets/img/streets.jpg",
+          source: new TileImage({
+            url: "http://mt1.google.com/vt/lyrs=m@113&hl=tr&&x={x}&y={y}&z={z}"
+          }),
+          visible: true
+        })
+      ],
+      target: "map",
+      view: new View({
+        center: [4000000, 4000000],
+        zoom: 4
+      })
+    });
+  }
+};
+</script>
+
+<style scoped>
+</style>
